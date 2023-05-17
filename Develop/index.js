17d61ec923006174ac0416fdfe885f7ee3a9638a
gh120515 <123872197+gh120515@users.noreply.github.com>
2023-05-17 18:36:26 +0930

    question list

--- Develop/index.js
+++ Develop/index.js
@@ -2,8 +2,66 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// sections: Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+
+// Array of questions for user input
+const questions = [{
+        type: 'input',
+        name: 'name',
+        message: 'What is your project (repository) title?',
+    },
+
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What is the description of your project?',
+    },
+
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the steps to install your project?',
+    },
+
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What are the usage steps for your project?',
+    },
+
+
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'How do you contribute to your project?',
+    },
+
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'How do you perform tests to your project?',
+    },
+
+    { 
+        type: "list",
+        name: "license",
+        message: "Please select the license(s) to your project.",
+        choices: ["GNU AGPLv3", "GNU GPLv3", "GNU LGPLv3", "Mozilla", "MIT", "Apache"],
+    },
+
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is your GitHub username?',
+    },
+
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+    },
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
